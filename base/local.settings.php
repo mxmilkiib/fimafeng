21a18af16c89b0f0ca88fc3b76e4bb49184337e2
aegir <aegir@purple.thingsandstuff.org>
2012-01-04 17:32:46 -0500

    moved base files to git submodules, updated readme

--- base/local.settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conf['cache'] = 0;				  // disable hardcoded cache
-$conf['preprocess_css'] = 0;      // disable hardcoded css aggregation
-$conf['preprocess_js'] = 0;       // disable hardcoded js aggregation
- ?>
