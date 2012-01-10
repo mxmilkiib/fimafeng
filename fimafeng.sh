ce8f47a72cd7131269dde0fa13cecdaab67d546b
root <root@purple.thingsandstuff.org>
2012-01-10 12:08:30 +0000

    various upgrades, interactive, remove project, d7core, etc.

--- fimafeng.sh
+++ fimafeng.sh
@@ -82,7 +82,7 @@ scriptprocess() {
 
   if [ "$SCRIPT_TASK" = "rc" ]; then removesite ; fi
   if [ "$SCRIPT_TASK" = "rb" ]; then removeplatform ; fi
-#  if [ "$SCRIPT_TASK" = "ra" ]; then removeproject ; fi
+  if [ "$SCRIPT_TASK" = "ra" ]; then removeproject ; fi
 }
 
 
@@ -110,8 +110,8 @@ getoptions() {
   DEFINE_string 'platform' '' 'Platform name (default is project name)' 'l'
   DEFINE_string 'domain' '' 'Site domain name' 'a'
   DEFINE_string 'task' '' 'Task to perform.
-            a -  Create project, build platform and provision site
-            b -  Use existing project, build platform and provision site
+            a - Create project, build platform and provision site
+            b - Use existing project, build platform and provision site
             c - Use existing project and platform, provision site
             rc - Removing site
             rb - Removing platform
@@ -120,7 +120,8 @@ getoptions() {
   DEFINE_boolean 'verbose' 'false' 'Verbose mode for Git and Drush' 'v'
   DEFINE_boolean 'usage' 'false' 'Usage string examples' 'u'
   DEFINE_boolean 'interactive' 'true' 'Prompt for continuation at points' 'i'
-  DEFINE_string 'theme' 'squaregrid' 'Base theme to use' 't'
+  DEFINE_string 'theme' 'DEFAULT' 'Base theme to use' 't'
+  DEFINE_string 'themebranch' '' 'Branch of theme to use' 'b'
 
   # parse the command-line
   FLAGS "$@" || exit 1
@@ -134,6 +135,7 @@ getoptions() {
   SCRIPT_TASK="${FLAGS_task}"                                                 # Required
   DRUPAL_VERSION="${FLAGS_drupal}"
   BASE_THEME="${FLAGS_theme}"
+  BASE_THEME_BRANCH="${FLAGS_themebranch}"
   VERBOSE_MODE="${FLAGS_verbose}"
   USAGE_MODE="${FLAGS_usage}"
   INTERACTIVE_MODE="${FLAGS_interactive}"
@@ -166,9 +168,9 @@ variablechecks() {
 
   #if [ -n  "$PLATFORM_NAME" ] ; then PLATFORM_NAME=$PROJECT_NAME ; fi       # If platform is not used, project name becomes platform name
 
-	if [ -n "$BASE_THEME" ] ; then                                            # If no theme is set, use garland for D6, squaregrid for D7
-  	if [ "$DRUPAL_VERSION" = "6" ]; then BASE_THEME="garland" ; fi
-	  if [ "$DRUPAL_VERSION" = "7" ]; then BASE_THEME="squaregrid" ; fi
+	if [ "$BASE_THEME" = "DEFAULT" ] ; then                                   # If no theme is set, use garland for D6, squaregrid for D7
+  	if [ "$DRUPAL_VERSION" = "6" ]; then BASE_THEME="om" ; BASE_THEME_BRANCH="6.x-2.x"  ; fi
+	  if [ "$DRUPAL_VERSION" = "7" ]; then BASE_THEME="sasson" ; BASE_THEME_BRANCH="7.x-2.0-beta3" ; fi
   fi
 
 	AEGIR_PATH=$HOME                                       	                  # The Aegir dir, typically /var/aegir
@@ -176,6 +178,8 @@ variablechecks() {
 
   if [ "$DRUPAL_VERSION" = "6" ]; then BASE_PROJECT=$FF_PATH/base/d6core.git/ ; fi
   if [ "$DRUPAL_VERSION" = "7" ]; then BASE_PROJECT=$FF_PATH/base/d7core.git/ ; fi
+
+  if [ ! -d "$BASE_PROJECT" ]; then echo "$BASE_PROJECT doesn't exist, exiting" ; fi
 
 	# Project profile Git folder for .make, .info, .profile and theme folders
  	PROJECT_PATH=$FF_PATH/projects/$PROJECT_NAME.git                          # /var/aegir/ff/projects/projectname.git
@@ -228,9 +232,7 @@ variablechecks() {
     echo " Site path: $SITE_PATH"
     VERBOSE_MODE="-v"
   else VERBOSE_MODE="" ; fi
-  echo ""
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
-  echo ""
+  ifinteractive
 }
 
 makeproject() {
@@ -263,22 +265,27 @@ makeproject() {
 	echo
 	echo "* Edit project .make and .info if required"
   echo
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
+  ifinteractive
 	echo
-	
-	# Setup subtheme
-	mkdir -p $SUB_THEME ; cd $SUB_THEME
-	echo "; $BASE_THEME subtheme" > $PROJECT_NAME.info
-	echo "" >> $PROJECT_NAME.info
-	echo "name = $PROJECT_NAME" >> $PROJECT_NAME.info
-	echo "description = $BASE_THEME subtheme for $PROJECT_NAME" >> $PROJECT_NAME.info
-  echo "core = $DRUPAL_VERSION.x" >> $PROJECT_NAME.info
-	echo "engine = phptemplate" >> $PROJECT_NAME.info
-	echo "base theme = $BASE_THEME" >> $PROJECT_NAME.info
-	echo "" >> $PROJECT_NAME.info
-	echo "stylesheets[all][] = css/$PROJECT_NAME.css" >> $PROJECT_NAME.info
-	mkdir css
-	touch css/$PROJECT_NAME.css
+
+
+	# Setup theme
+	mkdir -p $THEMES_PATH && cd $THEMES_PATH
+  git submodule add http://git.drupal.org/project/$BASE_THEME.git 
+  cd $BASE_THEME ; git checkout $BASE_THEME_BRANCH
+  
+
+#	echo "; $BASE_THEME subtheme" > $PROJECT_NAME.info
+#	echo "" >> $PROJECT_NAME.info
+#	echo "name = $PROJECT_NAME" >> $PROJECT_NAME.info
+#	echo "description = $BASE_THEME subtheme for $PROJECT_NAME" >> $PROJECT_NAME.info
+# echo "core = $DRUPAL_VERSION.x" >> $PROJECT_NAME.info
+#	echo "engine = phptemplate" >> $PROJECT_NAME.info
+#	echo "base theme = $BASE_THEME" >> $PROJECT_NAME.info
+#	echo "" >> $PROJECT_NAME.info
+#	echo "stylesheets[all][] = css/$PROJECT_NAME.css" >> $PROJECT_NAME.info
+#	mkdir css
+#	touch css/$PROJECT_NAME.css
 
 	# cp ../$BASE_THEME/template.php template.php
 	#	eval "sed -i s#YOURTHEMENAME#$PROJECT_NAME#g template.php"
@@ -286,7 +293,7 @@ makeproject() {
 	# Ask to manually edit the theme before comitting
 	echo "* Edit theme if so required"
   echo
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
+  ifinteractive
 	echo
 
   cd $PROJECT_PATH
@@ -314,22 +321,22 @@ aegirplatform() {
 	FF_STAGE=stage3_aegirthings_platform_files
 	echo "drush make --working-copy $PROJECT_MAKE $PLATFORM_PATH"
   echo
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; echo; fi
+  ifinteractive
 	drush make --working-copy "$PROJECT_MAKE" "$PLATFORM_PATH" "$VERBOSE_MODE"
 	echo
 
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; echo; fi
+  ifinteractive
 	# Set an Aegir context for that platform
 	FF_STAGE=stage4_aegirthings_platform_filesdrush
 	echo "drush provision-save "@platform_"$PLATFORM_NAME"" --root=""$PLATFORM_PATH"" --context_type=platform'"
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; echo; fi
+  ifinteractive
 	drush provision-save "@platform_"$PLATFORM_NAME"" --root="$PLATFORM_PATH" --context_type=platform "$VERBOSE_MODE"
 	drush @hostmaster hosting-dispatch
 	echo
 	
 	# Import that platform into hostmaster, the Aegir frontend
 	echo "drush @hostmaster hosting-import '@platform_"$PLATFORM_NAME"'"
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; echo; fi
+  ifinteractive
 	drush @hostmaster hosting-import "@platform_"$PLATFORM_NAME"" "$VERBOSE_MODE"
 	drush @hostmaster hosting-dispatch
 	echo
@@ -339,14 +346,12 @@ aegirsite() {
 	# Set a site context in Aegir using the new platform and profile
 	FF_STAGE=stage5_aegirthings_platform_filesdrushsite
 	echo "drush provision-save @$SITE_DOMAIN --uri='$SITE_DOMAIN' --context_type='site' --platform='@platform_"$PLATFORM_NAME"' --profile='$PROJECT_NAME' --db_server=@server_master" "$VERBOSE_MODE"
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
+  ifinteractive
 	drush provision-save "@$SITE_DOMAIN" --uri="$SITE_DOMAIN" --context_type='site' --platform="@platform_"$PLATFORM_NAME"" --profile="$PROJECT_NAME" "$VERBOSE_MODE" --debug
 
 	drush @hostmaster hosting-dispatch
 	echo
-	
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
-
+  ifinteractive
 	# Install site (init DB, etc.)
   # context then command
 	cd $PLATFORM_PATH
@@ -373,8 +378,8 @@ aegirsite() {
 # Removing things
 
 removesite() {
-	echo "*** Removing site: drush @hostmaster hosting-task @"$SITE_DOMAIN" delete --force"
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
+	echo "* Removing site: drush @hostmaster hosting-task @"$SITE_DOMAIN" delete --force"
+  ifinteractive
 	# Create delete site task in hostmaster
 	drush @hostmaster hosting-task @"$SITE_DOMAIN" delete --force --debug
 	drush @hostmaster hosting-dispatch
@@ -382,26 +387,30 @@ removesite() {
 
 
 removeplatform() {
-	echo "*** Removing platform: drush @hostmaster hosting-task @platform_"$PLATFORM_NAME" delete --force"
-  if [ "$INTERACTIVE_MODE" = "0" ]; then read -p "*** Press return to continue"; fi
+	echo "* Removing platform: drush @hostmaster hosting-task @platform_"$PLATFORM_NAME" delete --force"
+  ifinteractive
 	# Create delete platform task in hostmaster
 	drush @hostmaster hosting-task @platform_"$PLATFORM_NAME" delete --force --debug
 	drush @hostmaster hosting-dispatch --debug
-
-  #if [ -d $PLATFORM_PATH ]; then rm -rf $PLATFORM_PATH ; fi
 }
 
 
 removeproject() {
-	echo "*** Removing platform and project"
-
- 	#if [ -d $PROJECT_PATH ]; then rm -rf $PROJECT_PATH ; fi
-  #if [ -d $PLATFORM_PATH ]; then rm -rf $PLATFORM_PATH ; fi
+	echo "* Removing project folder"
+ 	if [ -d $PROJECT_PATH ]; then rm -rf $PROJECT_PATH ;
+  else echo "No project to remove"
+  fi
   echo
   exit
 }
 
-
+ifinteractive() {
+  if [ "$INTERACTIVE_MODE" = "0" ]; then 
+    echo ""
+    read -p "*** Press return to continue";
+    echo ""
+  fi
+}
 
 # Run the script
 scriptprocess "$@"
